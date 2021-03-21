bc4a269bd222cfaedba83ef3898cfdf2c1975d87
Syed Osama Hassan <syedosamahassan16@gmail.com>
2021-03-22 00:32:02 +0500

    updated analytics

--- src/Components/Analytics/Analytics.jsx
+++ src/Components/Analytics/Analytics.jsx
@@ -3,10 +3,12 @@ import NavBar from '../NavBar/NavBar'
 import Cards from "../Cards/Cards"
 import Charts from "../Charts/Charts";
 import firebase from '../../firebase';
+import { useAuth } from '../contexts/AuthContext';
 
 const db = firebase.database().ref();
 
 export default function Analytics() {
+    const { currentUser } = useAuth();
     // Initial data
     const [data, setData] = useState({
         "loan": "",
@@ -24,29 +26,38 @@ export default function Analytics() {
         var l;
         var f;
         var d;
+        var key;
+        var i;
         db.child('loan').on('value', snapshot => {
+            key = Object.entries(snapshot.val());
+            i = key[0][1].email;
+            if(i == currentUser.email){
             if (snapshot.val() != null) {
                 l = Object.keys(snapshot.val()).length
             }
-
+        }
         })
 
         //For Fund Raise
         db.child('fund-raise').on('value', snapshot => {
+            if(i == currentUser.email){
             if (snapshot.val() != null) {
                 f = Object.keys(snapshot.val()).length
             }
-
+        }
         })
         // For Draw
         db.child('draw').on('value', snapshot => {
+            if(i == currentUser.email){
             if (snapshot.val() != null) {
                 d = Object.keys(snapshot.val()).length
             }
+        }
         })
         //Setting data
         setData({ "loan": l, "fund": f, "Draw": d });
-    }
+    
+}
 
     return (
         <div>
